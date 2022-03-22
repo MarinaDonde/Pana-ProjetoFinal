15e0f5c80def71907f0aada131d8a1200ee04ea4
Marina Donde <madondee@hotmail.com>
2022-03-22 03:04:44 -0300

    add build

--- Pana/frontend/src/pages/Login/index.tsx
+++ Pana/frontend/src/pages/Login/index.tsx
@@ -1,21 +1,24 @@
+import { Link } from 'react-router-dom'
 import login from '../../img/login.png'
 import './login.css'
 
 const Login = () => {
   return (
     <section className="login">
-        <div>
-          <img className="img-login" src={login} alt="login" />
-        </div>
-        <div className="login-form">
-          <form className="form">
-            <h1>Bem vindo! Faça seu login:</h1>
-            <input type="email" className="form-control" placeholder="Email"></input>
-            <input type="password" className="form-control" placeholder="Senha"></input>
-            <p>Não tem conta? <a href="#">Clique aqui</a></p>
-            <button type="submit" className="btn-login">Entrar</button>
-          </form>
-        </div>
+      <div>
+        <img className="img-login" src={login} alt="login" />
+      </div>
+      <div className="login-form">
+        <form className="form">
+          <h1>Bem vindo! Faça seu login:</h1>
+          <input type="email" className="form-control" placeholder="Email"></input>
+          <input type="password" className="form-control" placeholder="Senha"></input>
+          <p>Não tem conta?
+            <Link to="Form">Clique aqui</Link>
+          </p>
+          <button type="submit" className="btn-login">Entrar</button>
+        </form>
+      </div>
     </section>
   )
 }
